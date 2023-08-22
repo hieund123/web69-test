6c52f77ddb7d45d9c0771596604dd64eaef3a756
hieund123 <butbisanh@gmail.com>
2023-08-22 23:51:35 +0700

    firsst

--- bai-2/node_review/db.js
+++ bai-2/node_review/db.js
@@ -2,43 +2,18 @@ const { MongoClient } = require("mongodb");
 
 const db = {};
 
-const connectToDb = () => {
+const connectToDb = async () => {
   const client = new MongoClient("mongodb://localhost:27017");
-  client.connect(async () => {
-    const database = client.db("your_db_name");
+  try {
+    await client.connect();
+    const database = client.db("database1");
     db.inventories = database.collection("inventories");
     db.orders = database.collection("orders");
     db.users = database.collection("users");
-
-    // Import Order data
-    const ordersData = [
-      { "_id": 1, "item": "almonds", "price": 12, "quantity": 2 },
-      { "_id": 2, "item": "pecans", "price": 20, "quantity": 1 },
-      { "_id": 3, "item": "pecans", "price": 20, "quantity": 3 },
-    ];
-    await db.orders.insertMany(ordersData);
-
-    // Import Inventory data
-    const inventoryData = [
-      { "_id": 1, "sku": "almonds", "description": "product 1", "instock": 120 },
-      { "_id": 2, "sku": "bread", "description": "product 2", "instock": 80 },
-      { "_id": 3, "sku": "cashews", "description": "product 3", "instock": 60 },
-      { "_id": 4, "sku": "pecans", "description": "product 4", "instock": 70 },
-    ];
-    await db.inventories.insertMany(inventoryData);
-
-    // Import Users data
-    const usersData = [
-      { "username": "admin", "password": "MindX@2022" },
-      { "username": "alice", "password": "MindX@2022" },
-    ];
-    await db.users.insertMany(usersData);
-
-    console.log("Data imported successfully.");
-
-    // Close the connection
-    client.close();
-  });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 module.exports = { connectToDb, db };
--- bai-2/node_review/index.js
+++ bai-2/node_review/index.js
@@ -1,9 +1,37 @@
-const express = require("express");
 const { connectToDb, db } = require("./db");
 
-const app = express();
+const ordersData = [
+  { "_id": 1, "item": "almonds", "price": 12, "quantity": 2 },
+  { "_id": 2, "item": "pecans", "price": 20, "quantity": 1 },
+  { "_id": 3, "item": "pecans", "price": 20, "quantity": 3 },
+];
 
-app.listen(3000, () => {
-  console.log("App is running at 3000");
-  connectToDb();
-});
+const inventoryData = [
+  { "_id": 1, "sku": "almonds", "description": "product 1", "instock": 120 },
+  { "_id": 2, "sku": "bread", "description": "product 2", "instock": 80 },
+  { "_id": 3, "sku": "cashews", "description": "product 3", "instock": 60 },
+  { "_id": 4, "sku": "pecans", "description": "product 4", "instock": 70 },
+];
+
+const usersData = [
+  { "username": "admin", "password": "MindX@2022" },
+  { "username": "alice", "password": "MindX@2022" },
+];
+
+const importData = async () => {
+  try {
+    await connectToDb();
+    await db.inventories.insertMany(inventoryData);
+    await db.orders.insertMany(ordersData);
+    await db.users.insertMany(usersData);
+    console.log("Data imported successfully");
+  } catch (error) {
+    console.error("Error importing data:", error);
+  } finally {
+    if (db.client) {
+      db.client.close(); 
+    }
+  }
+};
+
+importData();
